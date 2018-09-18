36928ae169eec10018569558f9f00f0599338c83
Jeevithan <jxevithan@outlook.com>
2018-09-18 08:38:52 +0200

    SaveLoad added and revised

--- Shakes and Fidget/SaveLoad.cs
+++ Shakes and Fidget/SaveLoad.cs
@@ -1,7 +1,35 @@
+using System;
+using System.IO;
+
 namespace Shakes_and_Fidget
 {
-    public class SaveLoad
+    public static class SaveLoad
     {
-        
+        public static void GameSave()
+        {
+            TextWriter tw = new StreamWriter("SavedGame.txt");
+
+            tw.WriteLine(Character.name);
+            tw.WriteLine(Performance.gold);
+            tw.WriteLine(Performance.experience);
+            tw.WriteLine(Performance.energy);
+
+            tw.Close();
+        }
+
+        public static void GameLoad()
+        {
+            TextReader tr = new StreamReader("SavedGame.txt");
+            Character.name = tr.ReadLine();
+            string goldString = tr.ReadLine();
+            string expString = tr.ReadLine();
+            string enrgyString = tr.ReadLine();
+
+            Performance.gold = Convert.ToInt32(goldString);
+            Performance.experience = Convert.ToInt32(expString);
+            Performance.energy = Convert.ToInt32(enrgyString);
+
+            tr.Close();
+        }
     }
 }
