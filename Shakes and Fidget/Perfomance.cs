7f88b17d0d57b0b968df44fe3750b5dd07f36b28
Harun Aksoy <harun.aksoy@csbe.ch>
2018-09-21 10:54:16 +0200

    Soundsystem wurde verbessert.

--- Shakes and Fidget/Perfomance.cs
+++ Shakes and Fidget/Perfomance.cs
@@ -13,10 +13,14 @@ namespace Shakes_and_Fidget
         public static int energy = 0;
         public static int questEnergy = 100;
 
+
         public static void CheckEnergy()
         {
-            SoundPlayer myPlayer = new SoundPlayer(@"C:\Users\vmadmin\RiderProjects\RPGProject\theme\theme.wav");
-
+            
+            
+                
+            
+            
             if (energy < Home.fullEnergy)
             {
                 Console.Clear();
@@ -30,8 +34,8 @@ namespace Shakes_and_Fidget
             else if (Level.Level1 == 1 || Level.Level1 == 2)
             {
                 energy -= questEnergy;
-                myPlayer.Play();
                 Stopwatch.Timer();
+                
             }
 
             else if (Level.Level1 == 3)
